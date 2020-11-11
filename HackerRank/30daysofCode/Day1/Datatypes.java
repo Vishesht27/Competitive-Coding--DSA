e8a072d8b677a46342e1aecbdae18a7a3422afc7
Vishesh <vishesht27@gmail.com>
2020-11-11 19:45:04 +0530

    Initial commit

---
+++ HackerRank/30daysofCode/Day1/Datatypes.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+         int i2 = scan.nextInt();
+         double d2 = scan.nextDouble();
+         scan.nextLine();
+         String s2= scan.nextLine();
+                 /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+         System.out.println(i+i2);
+         System.out.println(d+d2);
+         System.out.println(s+s2);
+
+        /* Print the sum of the double variables on a new line. */
+
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
---
+++ HackerRank/30daysofCode/Day1/Datatypes.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i1= int(input())
+d1=float(input())
+s1= input()
+print(i+i1)
+print(d+d1)
+print(s+s1)
