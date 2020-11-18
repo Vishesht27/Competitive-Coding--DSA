5713b85b26a15acde31407fc4a29db1450338dbd
Vishesh <vishesht27@gmail.com>
2020-11-18 11:33:53 +0530

    Initial Commit

---
+++ HackerRank/ProblemSolving/String/CamelCase.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+     String arr[]= s.split("[A-Z]");
+     return (arr.length);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ HackerRank/ProblemSolving/String/MarsExploration.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the marsExploration function below.
+    public static int marsExploration(String message) {
+     String sos ="SOS";
+     int count = 0;
+     for(int i=0;i < message.length();i++){
+         if(message.charAt(i)!=sos.charAt(i%3))count++;
+     }
+    return count++;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = marsExploration(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
