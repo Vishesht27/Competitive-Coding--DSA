fde292f92f6e6a063b24d912f0db295fd3a22841
Vishesh <vishesht27@gmail.com>
2020-11-20 10:47:09 +0530

    IC

---
+++ HackerRank/C++/Inputandoutput.java
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+     int a;
+     cin>>a;
+     int b;
+     cin>>b;
+     int c;
+     cin>>c;
+     int s;
+     s= a+b+c;
+     cout<<s<<endl;
+
+    return 0;
+}
