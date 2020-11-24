39f15717cd0134501201a29f7de7c81bee39c5a9
Vishesh <vishesht27@gmail.com>
2020-11-24 10:24:28 +0530

    Initial Commit

---
+++ CodeForces/ProblemsSet/1269A.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class codeforces{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(9*n);
+        System.out.println(8*n);
+    }
+}
