a7bedcaf92c1b1468912cfff46b15a295d4c3b3c
Vishesh <vishesht27@gmail.com>
2020-11-28 01:29:24 +0530

    Intial Commit

---
+++ KotlinBasicProblems/Addtwonumbers.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val read = Scanner(System.`in`)
+
+
+    val a = read.nextInt()
+
+    val b = read.nextInt()
+
+    val sum = a + b
+
+    println("$sum")
+}
