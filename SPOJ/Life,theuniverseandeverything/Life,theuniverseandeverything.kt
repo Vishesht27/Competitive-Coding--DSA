09605dffc7dcbef4618c33f455470ea22f477b2f
Vishesh <vishesht27@gmail.com>
2020-12-05 15:04:20 +0530

    Kotlin alos addded

---
+++ SPOJ/Life,theuniverseandeverything/Life,theuniverseandeverything.kt
@@ -0,0 +1,12 @@
+import java.util.*;
+fun main(){
+    var sc= Scanner(System.`in`)
+
+    while(true) {
+        var k=sc.nextInt()
+        if (k == 42) {
+            break;
+        }
+        println(k)
+    }
+}
