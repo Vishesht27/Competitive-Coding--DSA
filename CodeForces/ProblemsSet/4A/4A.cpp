1c7666b36d3e7d0d7d4de6ae30c5e90379a33e57
Vishesh <vishesht27@gmail.com>
2020-12-07 00:51:58 +0530

    cmon ,more kt and cpp

---
+++ CodeForces/ProblemsSet/4A/4A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  if(n>2){
+    if(n%2==0){
+      cout<<"YES"<<endl;
+    }else{
+      cout<<"NO"<<end;
+    }
+  }else{
+    cout<<"NO"<<endl;
+  }
+}
---
+++ CodeForces/ProblemsSet/4A/4A.kt
@@ -0,0 +1,14 @@
+import java.util.*;
+fun main(){
+    var sc =Scanner(System.`in`)
+    var w=sc.nextInt()
+    if(w>2){
+        if(w%2==0){
+            println("YES")
+        }else{
+            println("NO")
+        }
+    }else{
+        println("NO")
+    }
+}
