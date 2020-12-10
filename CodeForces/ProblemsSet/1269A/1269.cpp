eeabdffc4d3937ff8dfd23d9ee8de8634f136c16
Vishesh <vishesht27@gmail.com>
2020-12-10 20:09:27 +0530

    intial

---
+++ CodeForces/ProblemsSet/1269A/1269.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int n;
+cin>>n;
+cout<<n*9<<endl;
+cout<<8*n<<endl;
---
+++ CodeForces/ProblemsSet/4A/4A.py
@@ -0,0 +1,8 @@
+w=int(input())
+if w>2:
+    if w%2==0:
+        print("YES")
+    else:
+        print("NO")
+else:
+    print("NO")            
