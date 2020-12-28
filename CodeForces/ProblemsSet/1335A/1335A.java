93b658b6c0f5bd512ebd90e2d66452ced6dd18e0
Vishesh <vishesht27@gmail.com>
2020-12-28 19:39:11 +0530

    1335A

---
+++ CodeForces/ProblemsSet/1335A/1335A.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class candiesandsisters{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            long n=sc.nextLong();
+            System.out.println((n-1)/2);
+        }
+    }
+}
