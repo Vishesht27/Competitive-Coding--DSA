b7a00e620c026a72ab881444b1e0757b0043b8da
Vishesh <vishesht27@gmail.com>
2021-02-02 11:10:04 +0530

    implementattion problems

---
+++ HackerRank/ProblemSolving/Implementation/UtopianTree.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the utopianTree function below.
+    static int utopianTree(int n) {
+    int height=1;
+     if (x == 0){
+    h = 1
+     }
+else if (x % 2 == 0){
+    h = 2 ** ((x/2) + 1) - 1
+}
+else if(x % 2 == 1){
+    h = 2 ** ((x + 3)/2) - 2
+}
+return h
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
