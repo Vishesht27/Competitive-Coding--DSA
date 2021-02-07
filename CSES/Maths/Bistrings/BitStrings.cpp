88bfa1885afc5ee544a017321a97a2c2f95ae1e2
Vishesh <vishesht27@gmail.com>
2021-02-07 11:44:43 +0530

    new set added

--- CSES/Maths/Bistrings/BitStrings.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using name space std;
-signed main()
-{
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    int n; cin >> n;
-    cout << powMod(2, n) << '\n';
-    return 0;
-}
