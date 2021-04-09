d3d957373d03109bba6faa90b2c3abf8cd2c4d80
Vishesh <vishesht27@gmail.com>
2021-04-09 09:12:24 +0530

    Create LuckyBoundaries.java

---
+++ CodeChef/Problems/Beginner/LuckyBoundaries.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+class LuckyBoundaries {
+
+  static class FastReader {
+
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+  public static void main(String[] args) throws IOException {
+    FastReader sc = new FastReader();
+    int t = sc.nextInt();
+    while (t-- > 0) {
+      long n = sc.nextLong();
+      System.out.println(n);
+    }
+  }
+}
